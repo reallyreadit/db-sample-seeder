f5d8f012d1ff84c094d8a0938775c08a98d1f1bb
Thor Galle <thorgalle@gmail.com>
2022-06-23 14:42:56 +0300

    Add license

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// Copyright (C) 2022 reallyread.it, inc.
+//
+// This file is part of Readup.
+//
+// Readup is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+//
+// Readup is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License version 3 along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 using System.Text.RegularExpressions;
 using Npgsql;
 using Dapper;
@@ -168,7 +178,7 @@ namespace db_sample_seeder
 		}
 
 		// Below private methods are copy-pasted from the api repository
-		// should they be extracted into a shared utility package? 
+		// should they be extracted into a shared utility package?
 		// ----------------------------------------------------------------------------------------
 		private static string CreateSlug(string value)
 		{
